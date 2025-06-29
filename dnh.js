048c1512948b560ef8092c434390c627673a0803
Rhomulus <delomesounou@gmail.com>
2025-06-29 13:45:09 -0400

    Add files via upload

--- dnh.js
+++ dnh.js
@@ -1,1 +1,37 @@
 
+// categorie realisation: evenement , decoratipon
+
+let realiser= document.querySelector('.realo')
+realiser.addEventListener('click', (e)=>{
+e.preventDefault()
+let evenement = document.getElementById(even).value
+let decoration = document.getElementById(deco).value
+let choix
+if(choix== evenement){
+    document.getElementById("lien").innerHTML = realo
+}else if (choix == decoration){
+ document.getElementById("lion").innerHTML= readec
+}else{
+
+}
+})
+
+  function toggleContactOptions() {
+    const menu = document.getElementById("contactOptions");
+    //menu.style.display = menu.style.display === "block" ? "none" : "block";
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+      } else {
+        menu.style.display = "block";
+      }
+      
+  }
+  // Fermer le menu si on clique en dehors
+  window.onclick = function(event) {
+    const menu = document.getElementById("contactOptions");
+    if (!event.target.matches('.contact-menu button')) {
+      menu.style.display = "none";
+    }
+  }
+
+  
